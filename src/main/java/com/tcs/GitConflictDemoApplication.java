3978e0a7f558a1561441c186e457dbbbfcde9106
vishalmatale <you@example.com>
2022-08-16 22:37:01 +0530

    first project

---
+++ src/main/java/com/tcs/GitConflictDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConflictDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitConflictDemoApplication.class, args);
+	}
+
+}
